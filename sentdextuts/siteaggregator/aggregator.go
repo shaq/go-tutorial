14834d48e06a404397497f5a483fb317734b8e29
shaquizzle <shaquille.momoh@gmail.com>
2018-08-04 19:02:14 +0100

    using guardian instead of washington post. can make actually requests

--- sentdextuts/siteaggregator/aggregator.go
+++ sentdextuts/siteaggregator/aggregator.go
@@ -5,19 +5,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
 )
 
 // SitemapIndex represents <sitemap> tag of a sitemap.
-type SitemapIndex struct {
-	Locations []string `xml:"sitemap>loc"`
-}
+// type SitemapIndex struct {
+// 	Locations []string `xml:"sitemap>loc"`
+// }
 
 type News struct {
-	Titles    []string `xml:"url>news>title"`
-	Keywords  []string `xml:"url>news>keywords"`
-	Locations []string `xml:"url>loc"`
+	Titles   []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Location []string `xml:"url>loc"`
 }
 
 type NewsMap struct {
@@ -26,35 +24,40 @@ type NewsMap struct {
 }
 
 func main() {
-	var s SitemapIndex
+	// var s SitemapIndex
 	var n News
 	newsMap := make(map[string]NewsMap)
 
-	file := "washingtonpost.xml"
-	washPostXML, err := os.Open(file)
+	// file := "washingtonpost.xml"
+	// washPostXML, err := os.Open(file)
+	//
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	//
+	// fmt.Println("Successfully opened", file)
+	// defer washPostXML.Close()
+	//
+	// bytes, _ := ioutil.ReadAll(washPostXML)
 
+	// xml.Unmarshal(bytes, &s)
+	// for idx, loc := range s.Locations {
+	// 	s.Locations[idx] = strings.TrimSpace(loc)
+	// }
+
+	// for _, Loc := range s.Locations {
+	resp, err := http.Get("https://www.theguardian.com/sitemaps/news.xml")
+	fmt.Println(err)
+	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
+	xml.Unmarshal(bytes, &n)
 
-	fmt.Println("Successfully opened", file)
-	defer washPostXML.Close()
-
-	bytes, _ := ioutil.ReadAll(washPostXML)
-
-	xml.Unmarshal(bytes, &s)
-
-	for _, Loc := range s.Locations {
-		Loc = strings.TrimSpace(Loc)
-		resp, _ := http.Get(Loc)
-		// fmt.Println(err)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-
-		for idx := range n.Titles {
-			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
+	for idx := range n.Titles {
+		newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Location[idx]}
 	}
+	// }
 
 	for idx, data := range newsMap {
 		fmt.Println(idx, data.Keywords, data.Location)
