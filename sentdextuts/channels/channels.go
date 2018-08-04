1a31cf8c38d0ef77a847825d0dc745f603b3cf51
shaquizzle <shaquille.momoh@gmail.com>
2018-08-04 22:51:18 +0100

    definitely got channels down 💪🏾

--- sentdextuts/channels/channels.go
+++ sentdextuts/channels/channels.go
@@ -1,18 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func foo(c chan int, someValue int) {
+
+	defer wg.Done()
 	c <- someValue * 5
 }
 
+var wg sync.WaitGroup
+
 func main() {
-	fooVal := make(chan int)
+	// Create an int channel with a buffer of 10 to
+	// stop blocking.
+	fooVal := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		// Each time we create a goroutine, we'll add it to
+		// our WaitGroup for synchronisation.
+		wg.Add(1)
+		go foo(fooVal, i)
+	}
 
-	go foo(fooVal, 5)
-	go foo(fooVal, 3)
+	// Wait for all of the goroutines to load onto the channel
+	// before close it!
+	wg.Wait()
+	close(fooVal)
 
-	v1, v2 := <-fooVal, <-fooVal
-
-	fmt.Println(v1, v2)
+	// Read from the channel and print to stdout.
+	for val := range fooVal {
+		fmt.Println(val)
+	}
 }
