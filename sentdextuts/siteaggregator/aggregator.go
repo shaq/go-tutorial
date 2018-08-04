3c188c4c4f21ee8c216034051d711b7f0ee826d9
shaquizzle <shaquille.momoh@gmail.com>
2018-08-04 19:04:25 +0100

    removed redundancies from washington post

--- sentdextuts/siteaggregator/aggregator.go
+++ sentdextuts/siteaggregator/aggregator.go
@@ -7,11 +7,6 @@ import (
 	"net/http"
 )
 
-// SitemapIndex represents <sitemap> tag of a sitemap.
-// type SitemapIndex struct {
-// 	Locations []string `xml:"sitemap>loc"`
-// }
-
 type News struct {
 	Titles   []string `xml:"url>news>title"`
 	Keywords []string `xml:"url>news>keywords"`
@@ -24,40 +19,26 @@ type NewsMap struct {
 }
 
 func main() {
-	// var s SitemapIndex
 	var n News
 	newsMap := make(map[string]NewsMap)
 
-	// file := "washingtonpost.xml"
-	// washPostXML, err := os.Open(file)
-	//
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	//
-	// fmt.Println("Successfully opened", file)
-	// defer washPostXML.Close()
-	//
-	// bytes, _ := ioutil.ReadAll(washPostXML)
-
-	// xml.Unmarshal(bytes, &s)
-	// for idx, loc := range s.Locations {
-	// 	s.Locations[idx] = strings.TrimSpace(loc)
-	// }
-
-	// for _, Loc := range s.Locations {
 	resp, err := http.Get("https://www.theguardian.com/sitemaps/news.xml")
-	fmt.Println(err)
-	bytes, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	xml.Unmarshal(bytes, &n)
 
 	for idx := range n.Titles {
 		newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Location[idx]}
 	}
-	// }
 
 	for idx, data := range newsMap {
 		fmt.Println(idx, data.Keywords, data.Location)
