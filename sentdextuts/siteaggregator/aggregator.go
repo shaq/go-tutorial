cc3ad64fe174a706a71f67a99aae6e9b8e6a5a92
shaquizzle <shaquille.momoh@gmail.com>
2018-08-04 18:22:40 +0100

    moved xml to separate file

--- sentdextuts/siteaggregator/aggregator.go
+++ sentdextuts/siteaggregator/aggregator.go
@@ -5,122 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 )
-
-var washPostXML = []byte(`
-  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-politics-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-politics-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-opinions-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-opinions-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-local-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-local-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-sports-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-sports-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-national-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-national-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-world-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-world-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-business-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-business-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-technology-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-technology-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-lifestyle-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-lifestyle-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-entertainment-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-entertainment-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-blogs-goingoutguide-sitemap.xml
-  </loc>
-  </sitemap>
-  <sitemap>
-  <loc>
-  http://www.washingtonpost.com/news-goingoutguide-sitemap.xml
-  </loc>
-  </sitemap>
-  </sitemapindex>
-`)
 
 // SitemapIndex represents <sitemap> tag of a sitemap.
 type SitemapIndex struct {
@@ -128,22 +15,48 @@ type SitemapIndex struct {
 }
 
 type News struct {
-	Titles   []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>Keywords"`
-	Locs     []string `xml:"url>loc"`
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keywords string
+	Location string
 }
 
 func main() {
-	bytes := washPostXML
-
 	var s SitemapIndex
 	var n News
+	newsMap := make(map[string]NewsMap)
+
+	file := "washingtonpost.xml"
+	washPostXML, err := os.Open(file)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully opened", file)
+	defer washPostXML.Close()
+
+	bytes, _ := ioutil.ReadAll(washPostXML)
+
 	xml.Unmarshal(bytes, &s)
 
-	for _, loc := range s.Locations {
-		resp, _ := http.Get(loc)
+	for _, Loc := range s.Locations {
+		Loc = strings.TrimSpace(Loc)
+		resp, _ := http.Get(Loc)
+		// fmt.Println(err)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
-		fmt.Println(loc)
+
+		for idx := range n.Titles {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range newsMap {
+		fmt.Println(idx, data.Keywords, data.Location)
 	}
 }
